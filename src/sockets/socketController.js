c8bd40ac17f5dc598f3dcc80a216ae37b00b21e2
QED0711 <qdizon@gmail.com>
2020-06-13 19:20:24 -0400

    proof of concept for syncing multiple computer metronomes

--- src/sockets/socketController.js
+++ src/sockets/socketController.js
@@ -3,16 +3,40 @@ const sleep = ms => {
 }
 const socketController = {
 
-    handleComm: io => async data => {
-        for(let i = 0; i < 32; i++){
-            io.emit("test", {count: i})
-            console.log(new Date().getTime())
-            await sleep(500)
+    handlePing: io => async data => {
+        let pingCount = 0;
+        while(pingCount < 50){
+            await sleep(50)
+            await io.emit(`ping-${data.clientID}`, {serverTime: Date.now()})
+            pingCount += 1;
         }
+
+        io.emit(`sync-complete-${data.clientID}`, {message: "sync complete"})
+    },
+
+    handleMultiple: io => async data => {
+        let count = 1;
+        let currentTime = Date.now()
+        let newTime;
+        while (count <= 32) {
+            newTime = Date.now()
+            if(newTime - currentTime >= 500){
+                console.log({newTime, currentTime})
+                await io.emit("test", {count, time: newTime})
+                currentTime = newTime
+                count += 1
+            } 
+            await sleep(1);
+        }
+
     },
 
     handleSingle: io => async data => {
-        io.emit("test", {type: "downbeat"})
+        io.emit("test", { type: "downbeat" })
+    },
+
+    handleStartPerformance: io => data => {
+        io.emit("play", {startTime: Date.now() + data.delay})
     }
 
 }
--- src/sockets/sockets.js
+++ src/sockets/sockets.js
@@ -6,12 +6,15 @@ const sockets = io => {
 
     io.on('connection', socket => {
         
-        // console.log("SOMEONE CONNECTED TO THE SOCKET SERVER")
+        // console.log("NEW CONNECTION")
         
         // RECEIVING
-        socket.on("comm", socketController.handleComm(io))
-        socket.on("single", socketController.handleSingle(io))
+        socket.on("clientPing", socketController.handlePing(io))
 
+
+        socket.on("multiple", socketController.handleMultiple(io))
+        socket.on("single", socketController.handleSingle(io))
+        socket.on("start-performance", socketController.handleStartPerformance(io))
 
 
 
